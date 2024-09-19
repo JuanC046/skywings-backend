f64a47e3bd641d8df49a7999146328526338530e
juanc <juan.lopez15@utp.edu.co>
2024-09-19 15:02:27 -0500

    Created user login endpoint

---
+++ src/users/user.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Post, Body } from "@nestjs/common";
+import { UserService } from "./services/user.service";
+// import { AdminService } from "./services/admin.service";
+// import { Admin } from "./interfaces/admin.interface";
+// import { ClientService } from "./services/client.service";
+// import { Client } from "./interfaces/client.interface";
+import { User } from "./interfaces/user.interface";
+@Controller('users')
+export class UserController {
+    // constructor(private userService: UserService, private adminService: AdminService, private clientService: ClientService) {}
+
+    @Post('login')
+    login(@Body userData: User): string {
+        return this.userService.login(userData);
+    }
+}
