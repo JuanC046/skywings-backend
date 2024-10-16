c8e01f0d35eaa606983a07b45711edb038c6f8db
juanc <juan.lopez15@utp.edu.co>
2024-10-16 10:03:08 -0500

    Define flight interfaces

---
+++ src/flights/interfaces/flight.interface.ts
@@ -0,0 +1,27 @@
+export interface Flight {
+  code: string;
+  creator: string; // username admin
+  type: string; // National or International
+  origin: string;
+  destination: string;
+  priceFirstClass: number;
+  priceEconomyClass: number;
+  departureDate1: Date;
+  arrivalDate1?: Date;
+  departureDate2: Date;
+  arrivalDate2?: Date;
+  creationDate: Date;
+  lastUpdateDate?: Date;
+}
+
+export interface Seats {
+  flightCode: string;
+  totalSeats: number;
+  totalFirst: number;
+  totalTourist: number;
+  avaliableFirst: string;
+  avaliableTourist: string;
+  busyFirst: string;
+  busyTourist: string;
+  erased: boolean;
+}
